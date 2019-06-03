d400142cb4e6862d8c7317db579603a1cb55b269
Odin <kammerloher.andreas@gmail.com>
2019-06-03 22:03:50 +0200

    Move Discipline to DisciplineConfig, load disciplines from file

---
+++ src/main/kotlin/configs/DisciplineConfig.kt
@@ -0,0 +1,37 @@
+package configs
+
+import com.fasterxml.jackson.module.kotlin.readValue
+
+data class Discipline(
+    val name: String,
+    var level: Int,
+    val powersByLevel: Map<Int, List<DisciplinePower>>
+)
+
+enum class DisciplineName {
+    ANIMALISM,
+    AUSPEX,
+    CELERITY,
+    DOMINATE,
+    FORTITUDE,
+    OBFUSCATE,
+    POTENCE,
+    PRESENCE,
+    PROTEAN,
+    BLOOD_SORCERY,
+    RITUALS,
+    THIN_BLOOD_ALCHEMY
+}
+
+data class DisciplinePower(
+    val name: String,
+    val dicePool: String,
+    val cost: String,
+    val system: String,
+    val duration: String
+)
+
+fun loadDisciplines(): List<Discipline> {
+    val disciplinesText = Discipline::class.java.getResource("../disciplines.yaml").readText()
+    return configMapper.readValue(disciplinesText)
+}
