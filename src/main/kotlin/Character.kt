3d6f9bf70a2eb6db02fc63b20f59ab64a689c5e9
Odin <kammerloher.andreas@gmail.com>
2019-06-03 22:03:27 +0200

    Fill character class with proper attributes

--- src/main/kotlin/Character.kt
+++ src/main/kotlin/Character.kt
@@ -1,14 +1,16 @@
+import configs.Discipline
+
 data class Character(
     val attributes: Attributes,
     val skills: Skills,
-    val disciplines: List<DisciplinePower>,
+    val disciplines: List<Discipline>,
 
     var maxHealth: Int,
     var willpower: Int,
-    var humanity: Int,
     var bloodPotency: Int,
-
-    val levelsByDisciplines: Map<Discipline, Int>
+    var humanity: Int,
+    var experience: Int,
+    var generation: Int
 ) {
 
 }
