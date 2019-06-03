62cfee4ca83fd3cf2197b6e9d02857aa34153f3d
Odin <kammerloher.andreas@gmail.com>
2019-06-03 22:05:26 +0200

    Use actual disciplines, fix generateSkills sublist-ranges

--- src/main/kotlin/CharacterGenerator.kt
+++ src/main/kotlin/CharacterGenerator.kt
@@ -2,6 +2,7 @@ import configs.*
 
 class CharacterGenerator {
     private val defaultPointsToSpend = loadDefaultPointsToSpend()
+    private val disciplines = loadDisciplines()
     private val playerCharacterTypes = loadPlayerCharacterTypes()
     private val mortalCharacterTypes = loadMortalCharacterTypes()
     private val names = loadNames()
@@ -11,7 +12,7 @@ class CharacterGenerator {
 
         val attributes = generateAttributes(defaultPointsToSpend.attributes)
         val skills = generateSkills(defaultPointsToSpend.skillsByLevelOptions.getValue("balanced"))
-        val disciplines = generateDisciplinePowers(defaultPointsToSpend.disciplineLevels)
+        val disciplines = generateDisciplines(defaultPointsToSpend.disciplineLevels)
     }
 
     fun generateCharacter(): Character {
@@ -20,12 +21,24 @@ class CharacterGenerator {
         val attributes = generateAttributes(defaultPointsToSpend.attributes)
         val skills =
             generateSkills(defaultPointsToSpend.skillsByLevelOptions.getValue(skillDistributionOptions.random()))
-        val disciplines = generateDisciplinePowers(defaultPointsToSpend.disciplineLevels)
+        val disciplines = generateDisciplines(defaultPointsToSpend.disciplineLevels)
 
         val health = 3 + attributes.physical.getValue(PhysicalAttribute.STAMINA) // + fortitude discipline bonus
+        val characterType = playerCharacterTypes.values.random()
+        val willpower =
+            attributes.social.getValue(SocialAttribute.COMPOSURE) + attributes.mental.getValue(MentalAttribute.RESOLVE)
 
-        // TODO: fill with actual values
-        return Character(attributes, skills, listOf(), health, -1, -1, -1, mapOf())
+        return Character(
+            attributes,
+            skills,
+            disciplines,
+            health,
+            willpower,
+            characterType.bloodPotency,
+            characterType.humanity,
+            characterType.experience,
+            characterType.generations.random()
+        )
     }
 
     fun generateName(): Pair<String, String> {
@@ -51,15 +64,25 @@ class CharacterGenerator {
     fun generateSkills(pointsByLevel: PointsByLevel): Skills {
         val pointList = pointsByLevelToPointList(pointsByLevel).fillWith(0, totalSkillCount).shuffled()
 
-        val physical = physicalSkillsFromList(pointList.subList(0, 7))
-        val social = socialSkillsFromList(pointList.subList(8, 15))
-        val mental = mentalSkillsFromList(pointList.subList(16, 23))
+        val physical = physicalSkillsFromList(pointList.subList(0, 9))
+        val social = socialSkillsFromList(pointList.subList(9, 18))
+        val mental = mentalSkillsFromList(pointList.subList(18, 27))
 
         return Skills(physical, social, mental)
     }
 
-    fun generateDisciplinePowers(levels: List<Int>): List<DisciplinePower> {
-        // TODO: generate actual discipline values
+    fun generateDisciplines(levels: List<Int>): List<Discipline> {
+        val pickedDisciplines = mutableListOf<Discipline>()
+        for (level in 1..levels.size) {
+            pickedDisciplines.add((disciplines - pickedDisciplines).random())
+        }
+        pickedDisciplines.shuffle()
+
+        for (level in levels) {
+
+        }
+
+
         return listOf()
     }
 
