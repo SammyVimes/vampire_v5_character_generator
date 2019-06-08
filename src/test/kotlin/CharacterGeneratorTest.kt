d09cd4baa20d3882a58ceb2f62bfd99a748aab34
Odin <kammerloher.andreas@gmail.com>
2019-06-08 15:56:17 +0200

    Add tests for CharacterGenerator, refactor PointsToSpend tests

--- src/test/kotlin/CharacterGeneratorTest.kt
+++ src/test/kotlin/CharacterGeneratorTest.kt
@@ -4,14 +4,27 @@ import org.junit.jupiter.api.Test
 internal class CharacterGeneratorTest {
 
     @Test
+    fun smokeTestCharacterGeneration() {
+        val character = CharacterGenerator().generateCharacter()
+    }
+
+    @Test
     fun testGenerateSkills() {
         val characterGenerator = CharacterGenerator()
         val defaultPointsToSpend = loadDefaultPointsToSpend()
 
         for (skillDistribution in skillDistributionOptions) {
-            val skills =
-                characterGenerator.generateSkills(defaultPointsToSpend.skillsByLevelOptions.getValue(skillDistribution))
+            val availablePointsByLevel = defaultPointsToSpend.skillsByLevelOptions.getValue(skillDistribution)
+            val skills = characterGenerator.generateSkills(availablePointsByLevel)
 
+            assert(skills.physical.values.all { it in 0..5 })
+            assert(skills.social.values.all { it in 0..5 })
+            assert(skills.mental.values.all { it in 0..5 })
+
+            val physicalSum = skills.physical.values.sum()
+            val socialSum = skills.social.values.sum()
+            val mentalSum = skills.mental.values.sum()
+            assert(physicalSum + socialSum + mentalSum == availablePointsByLevel.map { it.key * it.value }.sum())
             // TODO: assert sanity for skill distribution
         }
     }
--- src/test/kotlin/configs/PointsToSpendConfigKtTest.kt
+++ src/test/kotlin/configs/PointsToSpendConfigKtTest.kt
@@ -4,17 +4,39 @@ import org.junit.jupiter.api.Test
 
 internal class PointsToSpendConfigKtTest {
     @Test
-    fun defaultPointsAreSane() {
+    fun defaultAttributePointsAreSane() {
         val defaultPointsToSpend = loadDefaultPointsToSpend()
 
         with(defaultPointsToSpend) {
             assert(attributes.all { it.key in 1..5 })
             assert(attributes.map { it.value }.sum() == 9)
+        }
+    }
 
+    @Test
+    fun defaultSkillPointsAreSane() {
+        val defaultPointsToSpend = loadDefaultPointsToSpend()
+
+        with(defaultPointsToSpend) {
             assert(skillsByLevelOptions.all { it.value.all { skillsByLevel -> skillsByLevel.key in 1..5 } })
             assert(skillsByLevelOptions.all { it.value.map { skillsByLevel -> skillsByLevel.value }.sum() <= 24 })
+        }
+    }
 
+    @Test
+    fun defaultDisciplinePointsAreSane() {
+        val defaultPointsToSpend = loadDefaultPointsToSpend()
+
+        with(defaultPointsToSpend) {
             assert(disciplineLevels.all { it in 1..5 })
+        }
+    }
+
+    @Test
+    fun defaultAdvantagePointsAreSane() {
+        val defaultPointsToSpend = loadDefaultPointsToSpend()
+
+        with(defaultPointsToSpend) {
             assert(advantages >= 0)
             assert(flaws >= 0)
         }
