cd4e0c9f11b7269f78c739987b49687d80c46212
Odin <kammerloher.andreas@gmail.com>
2019-06-10 15:03:23 +0200

    Add junit dependency properly

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,14 @@ dependencies {
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.9.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.11.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
